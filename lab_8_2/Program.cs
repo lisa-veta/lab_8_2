21174531a1d0f3ac8aa65a3f3d1856937ceb8bc9
Elizaveta Pernikova <lisapernikowa@mail.ru>
2022-12-16 00:09:58 +0500

    рефакторинг 1.0

--- lab_8_2/Program.cs
+++ lab_8_2/Program.cs
@@ -16,40 +16,43 @@ namespace lab8_2
         static int start = 0;
         static void ReadData()
         {
-            foreach (string str in File.ReadAllLines("D:\\УНИВЕР\\прога\\лб8\\8-22.txt"))
+            foreach (string str in File.ReadAllLines("D:\\УНИВЕР\\прога\\лб8\\8-2.txt"))
             {
                 if(start == 0)
                 {
                     start = int.Parse(str);
                     continue;
                 }
-                Bill line = new Bill();
-                string[] stri = str.Split('|');
-                if (stri.Length > 2)
+
+                Bill BillLine = new Bill();
+                string[] line = str.Split('|');
+
+                if (line.Length > 2)
                 {
-                    line.Date = long.Parse(stri[0].Replace(":", "").Replace("-", "").Replace(" ", ""));
-                    line.Count = int.Parse(stri[1].Trim(' '));
-                    line.Operation = stri[2].Replace(" ", "");
+                    BillLine.Date = long.Parse(line[0].Replace(":", "").Replace("-", "").Replace(" ", ""));
+                    BillLine.Count = int.Parse(line[1].Trim(' '));
+                    BillLine.Operation = line[2].Replace(" ", "");
                 }
                 else
                 {
-                    line.Date = long.Parse(stri[0].Replace(":", "").Replace("-", "").Replace(" ", ""));
-                    line.Count = 0;
-                    line.Operation = stri[1].Replace(" ", "");
+                    BillLine.Date = long.Parse(line[0].Replace(":", "").Replace("-", "").Replace(" ", ""));
+                    BillLine.Count = 0;
+                    BillLine.Operation = line[1].Replace(" ", "");
                 }
-                billData.Add(line);
+                billData.Add(BillLine);
             }
+
             billData.Sort(delegate (Bill x, Bill y) { return x.Date.CompareTo(y.Date); } );
         }
 
         static int StartWork(string dateNow)
         {
-            long str = long.Parse(dateNow.Replace(":", "").Replace("-", "").Replace(" ", ""));
+            long entereDate = long.Parse(dateNow.Replace(":", "").Replace("-", "").Replace(" ", ""));
             int billNow = start;
 
             for (int i = 0; i < billData.Count; i++)
             {
-                if (str >= billData[i].Date)
+                if (entereDate >= billData[i].Date)
                 {
                     switch (billData[i].Operation)
                     {
@@ -70,22 +73,33 @@ namespace lab8_2
 
         static int Revert(int count, string operation, int billNow)
         {
-            if (operation == "in")
-                return billNow - count;
-            else
-                return billNow + count;
+            switch (operation)
+            {
+                case "in":
+                    return billNow - count;
+                    break;
+                case "out":
+                    return billNow + count;
+                    break;
+                case "revert":
+                    throw new Exception("You can't revert twice. Possibly invalid data in file");
+                    break;
+                default:
+                    throw new Exception("Invalid data in file");
+                    break;
+            }
         }
         
         static void Main()
         {
             ReadData();
-            Console.Write("Enter the date: ");
+            Console.Write("Enter the date in the format yyyy-mm-dd: ");
             string dateNow = Console.ReadLine();
             int bill = StartWork(dateNow);
             if (bill >= 0)
-                Console.WriteLine($"\nАccount balance: {bill}");
+                Console.WriteLine($"\nАccount balance as of {dateNow}: {bill}");
             else
-                Console.WriteLine("Invalid data in file");
+                throw new Exception("Invalid data in file");
         }
     }
 }
